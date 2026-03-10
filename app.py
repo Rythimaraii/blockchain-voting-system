2e8cd999a94981126df9b52467e52e87d8a086f8
Rythimaraii <rrythima@gmail.com>
2026-03-10 15:00:58 -0700

    Fix app.py to use Render PORT environment variable

--- app.py
+++ app.py
@@ -150,5 +150,8 @@ def vote():
     return render_template("vote.html")
 
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # use Render’s PORT or default 5000
+    app.run(host="0.0.0.0", port=port)
